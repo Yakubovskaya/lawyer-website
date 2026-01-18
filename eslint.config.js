c0c75657ef5943fcf9586cb62e37890346649e92
Marina Yakubovskaya <maribars93@yandex.ru>
2026-01-19 00:43:02 +0300

    chore: set up linters, husky, and css folder structure

---
+++ eslint.config.js
@@ -0,0 +1,16 @@
+import js from '@eslint/js';
+import globals from 'globals';
+import prettier from 'eslint-config-prettier';
+
+export default [
+  js.configs.recommended,
+  prettier,
+  {
+    files: ['**/*.{js,mjs,cjs}'],
+    languageOptions: {
+      globals: globals.browser,
+      ecmaVersion: 'latest',
+      sourceType: 'module',
+    },
+  },
+];
